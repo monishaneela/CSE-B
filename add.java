6b612c6e0cc55a0858055f91b422e2aeb115eed3
monishaneela <monishaneela27@gmail.com>
2025-07-25 13:48:28 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,5 +2,6 @@ class add{
   public static void main(String[]args){
     int a=15,b=60;
     sum=a+b;
-    system.put.println("the sum of a and b:"+sum);
-  }}
+    system.out.println("the sum of a and b:"+sum);
+    system.out.println("hello monisha")
+
